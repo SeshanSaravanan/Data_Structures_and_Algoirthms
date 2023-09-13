75b9348ef546d6b19e01cc890684f90d93269f21
SeshanSaravanan <88974719+SeshanSaravanan@users.noreply.github.com>
2023-09-13 22:18:21 +0530

    Create main.java

---
+++ Arrays and ArraysLists /17. Determine Whether matrix can be obtained by rotation/main.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        
+        for (int i = 0; i < 4; i++){
+            if ( same(mat, target) ){
+                return true;
+            }
+            mat = rotate(mat);
+        }
+        return false;
+    }
+
+    public boolean same(int[][] mat, int[][] target){
+        if (mat.length == target.length && mat[0].length == target[0].length){
+            for (int i = 0; i < mat.length; i++){
+                for (int j = 0; j < mat[0].length; j++){
+                    if (mat[i][j] != target[i][j]){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+
+    public int[][] rotate (int[][] mat){
+        int n = mat.length;
+        int m = mat[0].length;
+
+        int[][] b = new int[n][m];
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                b[j][n-i-1] = mat[i][j];
+            }
+        }
+        return b;
+    }
+}
